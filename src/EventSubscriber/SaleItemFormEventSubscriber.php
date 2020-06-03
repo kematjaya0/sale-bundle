ac27ce004ab8915870f0ac264ee386fe84a64315
Nur Hidayatullah <36223951+kematjaya0@users.noreply.github.com>
2020-06-03 21:20:57 +0700

    add form constraint

--- src/EventSubscriber/SaleItemFormEventSubscriber.php
+++ src/EventSubscriber/SaleItemFormEventSubscriber.php
@@ -5,18 +5,27 @@ namespace Kematjaya\SaleBundle\EventSubscriber;
 use Kematjaya\SaleBundle\Entity\SaleItemInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @author Nur Hidayatullah <kematjaya0@gmail.com>
  */
 class SaleItemFormEventSubscriber implements EventSubscriberInterface
 {
+    private $translator;
+    
+    public function __construct(TranslatorInterface $translator) 
+    {
+        $this->translator = $translator;
+    }
     
     public static function getSubscribedEvents()
     {
         return [
-            FormEvents::SUBMIT => 'onSubmit'
+            FormEvents::SUBMIT => 'onSubmit',
+            FormEvents::POST_SUBMIT => 'postSubmit'
         ];
     }
     
@@ -29,5 +38,31 @@ class SaleItemFormEventSubscriber implements EventSubscriberInterface
         }
         
         $event->setData($data);
+    }
+    
+    public function postSubmit(FormEvent $event)
+    {
+        $data = $event->getData();
+        if($data instanceof SaleItemInterface)
+        {
+            $item = $data->getItem();
+            $form = $event->getForm();
+            if($item->getLastStock() < $data->getQuantity())
+            {
+                $form->get('quantity')->addError(new FormError($this->translator->trans('stock_tidak_cukup').', Max: '. $item->getLastStock()));
+                return;
+            }
+            
+            if($item->getLastPrice() <= 0)
+            {
+                $form->get('sale_price')->addError(new FormError($this->translator->trans('sale_price_null')));
+                return;
+            }
+            
+            if($item->getLastPrice() <= $item->getPrincipalPrice())
+            {
+                $form->get('sale_price')->addError(new FormError($this->translator->trans('sale_price_less_than_principal_price')));
+            }
+        }
     }
 }
