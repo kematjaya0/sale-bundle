73ab439ce81166e3c698321b2e47f5a191546599
Nur Hidayatullah <kematjaya0@gmail.com>
2021-04-04 11:31:25 +0700

    remove subscriber

--- src/EventSubscriber/SaleItemFormEventSubscriber.php
+++ src/EventSubscriber/SaleItemFormEventSubscriber.php
@@ -25,10 +25,12 @@ class SaleItemFormEventSubscriber implements EventSubscriberInterface
     public function onSubmit(FormEvent $event)
     {
         $data = $event->getData();
-        if($data instanceof SaleItemInterface)
-        {
-            $data->setPrincipalPrice($data->getItem()->getPrincipalPrice());
+        if (!$data instanceof SaleItemInterface) {
+            
+            return;
         }
+        
+        $data->setPrincipalPrice($data->getItem()->getPrincipalPrice());
         
         $event->setData($data);
     }
@@ -43,18 +45,28 @@ class SaleItemFormEventSubscriber implements EventSubscriberInterface
         $item = $data->getItem();
         $form = $event->getForm();
         if($item->getLastStock() < $data->getQuantity()) {
-            $form->get('quantity')->addError(new FormError(sprintf('not sufficient stock for product %s, available: %s', $item->getName(), $item->getLastStock())));
+            $form->get('quantity')
+                    ->addError(new FormError(sprintf('not sufficient stock for product %s, available: %s', $item->getName(), $item->getLastStock())));
             return;
         }
 
         if($item->getLastPrice() <= 0) {
-            $form->get('sale_price')->addError(new FormError(sprintf('price not set for item %s', $item->getName())));
+            $form->get('sale_price')
+                    ->addError(new FormError(sprintf('price not set for item %s', $item->getName())));
             return;
         }
 
         if($item->getLastPrice() <= $item->getPrincipalPrice()) {
-            $form->get('sale_price')->addError(new FormError(sprintf('sale price (%s) less than principal price (%s), please contact administrator.', $item->getLastPrice(), $item->getPrincipalPrice())));
+            $form->get('sale_price')
+                    ->addError(new FormError(sprintf('sale price (%s) less than principal price (%s), please contact administrator.', $item->getLastPrice(), $item->getPrincipalPrice())));
             return;
         }
+        
+        $subTotal = $data->getQuantity() * $data->getSalePrice();
+        $total = $subTotal + $data->getTax() - $data->getDiscount();
+        $data->setSubTotal($subTotal);
+        $data->setTotal($total);
+        
+        $event->setData($data);
     }
 }
